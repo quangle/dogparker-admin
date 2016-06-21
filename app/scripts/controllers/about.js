f7f081715d68c26772a565f9d5192ad72713fe59
Quang Le & Duc Pham <quang.le+duc.pham@eastagile.com>
2016-06-21 16:14:27 +0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dogparkerAdminApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dogparkerAdminApp
+ */
+angular.module('dogparkerAdminApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dogparkerAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dogparkerAdminApp
+ */
+angular.module('dogparkerAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
